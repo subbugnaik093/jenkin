e0ecbee957730b6e00325530b71f2ea54ff5ee4e
unknown <subbugnaik093@gmail.com>
2023-05-25 22:35:53 +0530

    First time

---
+++ Sample.java
@@ -0,0 +1,10 @@
+class Sample{
+public static void main(String[]args){
+System.out.println("Hello java");
+}
+}:
+class Sample{
+public static void main(String[]args){
+System.out.println("Hello java");
+}
+}
